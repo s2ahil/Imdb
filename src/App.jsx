d8c3a676695d32ca41f9253ce1357efa7ca935c8
s2ahil <sahilpradhan411@gmail.com>
2025-01-11 13:27:07 +0530

    added task

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import { useState } from 'react'
 
-
+import ItemListManager from './components/ItemListManager'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+
+
     <>
-      <div className='flex flex-col justify-center items-center bg-green-100 h-[100vh] gap-2'>
-          <div className='text-5xl'>4
-          <span>0</span>
-          4</div>
-          <div className='text-5xl'>Page Not Found</div>
-      </div>
+      <ItemListManager></ItemListManager>
 
     </>
   )
--- src/components/ManagePages.js
+++
@@ -1,5 +0,0 @@
-import Appbar from "./Appbar";
-
-export {
-    Appbar,
-}
